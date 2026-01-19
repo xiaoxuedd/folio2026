b4db433a7e5dd61815829b71f52341bc07cbbbf2
Xiaoxue Dong <xuexue@Mac-mini.local>
2026-01-19 08:09:11 +0000

    updating workflow.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.designdoings.com',
-  base: '/',
+  site: 'https://xiaoxuedd.github.io',
+  base: '/folio2026/',
   integrations: [react()],
   output: 'static',
   build: {
